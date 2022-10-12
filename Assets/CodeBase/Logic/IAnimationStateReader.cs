9ddd5eda6c86398ad41739b55d3076a528a557cd
AlexseyKostiunin <85487222+AlexseyKostiunin@users.noreply.github.com>
2022-10-12 16:34:51 +0300

    Chapter 1 lesson 5

---
+++ Assets/CodeBase/Logic/IAnimationStateReader.cs
@@ -0,0 +1,10 @@
+namespace CodeBase.Logic
+{
+    public interface IAnimationStateReader
+    {
+        void EnteredState(int stateHash);
+        void ExitedState(int stateHash);
+
+        AnimatorState State { get; }
+    }
+}
--- Assets/CodeBase/Services/Constants.cs
+++ Assets/CodeBase/Services/Constants.cs
@@ -2,9 +2,7 @@ namespace CodeBase.Services
 {
     public static class Constants
     {
-        public const string Horizontal = "Horizontal";
-        public const string Vertical = "Vertical";
-        public const string Fire = "Fire";
+        public const float Epsilon = 0.001f;
     }
 }
     
